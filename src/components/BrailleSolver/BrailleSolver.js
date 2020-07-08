a30b5a68afb50ad793f58304eab8f106fac5b54b
Lizzie Paris <me@lizzieparis.com>
2020-07-08 17:58:54 -0400

    Use Hooks to control Checkbox values and get result

--- src/components/BrailleSolver/BrailleSolver.js
+++ src/components/BrailleSolver/BrailleSolver.js
@@ -1,14 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
-import { lookupBraille } from './BrailleSolver.utils';
+import { lookupBraille, getBinary } from './BrailleSolver.utils';
 import classes from './BrailleSolver.module.scss';
 
+const setterGetter = (valX, setValX) => () => setValX(getBinary(!valX));
+
+function Checkbox ({ value, stateHandler }) {
+  return (
+    <input type="checkbox" value={value} onChange={setterGetter(value, stateHandler)} />
+  );
+}
+
 function BrailleSolver() {
+  const [val0, setVal0] = useState(0);
+  const [val1, setVal1] = useState(0);
+  const [val2, setVal2] = useState(0);
+  const [val3, setVal3] = useState(0);
+  const [val4, setVal4] = useState(0);
+  const [val5, setVal5] = useState(0);
+
+  const [result, setResult] = useState('');
+
+  const getResult = (e) => {
+    e.preventDefault();
+    const fullVal = [val5, val4, val3, val2, val1, val0].join('');
+    setResult(lookupBraille(fullVal) || 'Not a valid Braille encoding');
+  };
+
+  const clearInputs = () => {
+    setVal0(false);
+    setVal1(false);
+    setVal2(false);
+    setVal3(false);
+    setVal4(false);
+    setVal5(false);
+    setResult('');
+  };
+
   return (
     <div className={classes.container}>
       <h1>BrailleSolver</h1>
       <div className={classes.inputs}>
-        {lookupBraille('100000')}
+        <form id="braille-form">
+          <div className={classes.row}>
+            <Checkbox value={val5} stateHandler={setVal5} />
+            <Checkbox value={val4} stateHandler={setVal4} />
+          </div>
+          <div className={classes.row}>
+            <Checkbox value={val3} stateHandler={setVal3} />
+            <Checkbox value={val2} stateHandler={setVal2} />
+          </div>
+          <div className={classes.row}>
+            <Checkbox value={val1} stateHandler={setVal1} />
+            <Checkbox value={val0} stateHandler={setVal0} />
+          </div>
+          <button type="submit" onClick={getResult}>Submit</button>
+          <button type="reset" onClick={clearInputs}>Reset</button>
+        </form>
+        <h2>{result}</h2>
       </div>
     </div>
   );
--- src/components/BrailleSolver/BrailleSolver.utils.js
+++ src/components/BrailleSolver/BrailleSolver.utils.js
@@ -1,9 +1,13 @@
 import findKey from 'lodash/findKey';
+import sample from 'lodash/sample';
 // import pick from 'lodash/pick';
+
 import { latinToBraille } from '../../constants';
 
-export const lookupBraille = (formResults) => {
-  const inputBrailleBin = formResults;
+export const getBinary = (val) => (val ? 1 : 0);
+
+export const lookupBraille = (inputBrailleBin) => {
+  console.log('lzz looking up', inputBrailleBin, typeof inputBrailleBin, typeof sample(latinToBraille).brailleBin)
   const latinLetter = findKey(latinToBraille, ({ brailleBin }) => brailleBin === inputBrailleBin);
   return latinLetter;
 };
