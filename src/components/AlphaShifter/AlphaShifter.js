ae0d10ea8f5f0d345ccabeda0b5f5f1c818acbaa
Lizzie Paris <me@lizzieparis.com>
2020-07-10 18:28:09 -0400

    Create AlphaShifter component

---
+++ src/components/AlphaShifter/AlphaShifter.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import classes from './AlphaShifter.module.scss';
+
+function AlphaShifter({ themeColor }) {
+  const [inputValue, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const getResult = (e) => {
+    e.preventDefault();
+    setResult('pie');
+  };
+
+  const clearInput = () => setInput('');
+
+  return (
+    <div className={classes.container}>
+      <h1>Alpha Shifter</h1>
+      <p className={classes.result}>{result}</p>
+      <div className={classes.inputs}>
+        <form id="alpha-shifter-form">
+          <label><input type="text" value={inputValue} onChange={(e) => setInput(e.target.value)} /></label>
+          <div className={classes.row}>
+            <button className="outline" type="reset" onClick={clearInput}>Reset</button>
+            <button type="submit" onClick={getResult} style={{ color: themeColor }}>Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default AlphaShifter;
